ec4d8c97c470710a2ecf5e762e9e175c777b78ec
Mahmoudalbash <PS236927@edu.summacollege.nl>
2025-01-08 16:48:47 +0100

    menu pagina met images

--- app/Models/Pizza.php
+++ app/Models/Pizza.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -8,7 +9,7 @@ class Pizza extends Model
 {
     use HasFactory;
 
-    protected $table = 'pizza'; // Verwijst naar de 'pizza'-tabel
+    protected $table = 'pizza';
 
     protected $fillable = [
         'name',
