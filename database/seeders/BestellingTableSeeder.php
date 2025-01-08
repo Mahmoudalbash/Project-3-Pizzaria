64162da7d55936351598098af818984a4866f5b0
Khalil <ps250618@summacollege.nl>
2025-01-08 15:32:24 +0100

    seeders en modellen gemaakt

---
+++ database/seeders/BestellingTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BestellingTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
